e5c0dee81208c101711305d5031508871816ed84
Aseer Tayeem <61358187+atayeem@users.noreply.github.com>
2025-06-09 14:58:24 -0400

    Update script.js

--- ics/dance/script.js
+++ ics/dance/script.js
@@ -170,7 +170,7 @@ function drawPose(pose, explode) {
     if (webcam.canvas) {
         ctx.drawImage(webcam.canvas, 0, 0);
         if (pose) {
-            const minPartConfidence = 0.5;
+            const minPartConfidence = 0.2;
             if (explode) {
                 pose.keypoints.forEach(keypoint => {
                     if (keypoint.score > minPartConfidence) {
